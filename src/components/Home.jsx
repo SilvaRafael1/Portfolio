0135a2b339caed219dbaf15828d17c1f5d837e3f
SilvaRafael1 <rafaelaugustodasilva2@gmail.com>
2023-03-27 16:15:26 -0300

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/assets/css.png 	new file:   src/assets/github.png 	new file:   src/assets/graphql.png 	new file:   src/assets/heroImage.jpeg 	new file:   src/assets/html.png 	new file:   src/assets/icon.jpg 	new file:   src/assets/javascript.png 	new file:   src/assets/nextjs.png 	new file:   src/assets/node.png 	new file:   src/assets/portfolio/arrayDestruct.jpg 	new file:   src/assets/portfolio/installNode.jpg 	new file:   src/assets/portfolio/navbar.jpg 	new file:   src/assets/portfolio/reactParallax.jpg 	new file:   src/assets/portfolio/reactSmooth.jpg 	new file:   src/assets/portfolio/reactWeather.jpg 	new file:   src/assets/portfolio/usestate.jpg 	new file:   src/assets/react.png 	new file:   src/assets/tailwind.png 	new file:   src/components/Home.jsx 	new file:   src/components/NavBar.jsx 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   tailwind.config.js

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from '../assets/heroImage.jpeg';
+
+const Home = () => {
+  return (
+    <div>
+        <img src={Image} alt="" />
+    </div>
+  )
+}
+
+export default Home
